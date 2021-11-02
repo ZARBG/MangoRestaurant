e43c6b2a9de6cd7810115fb471f53795b435685c
Guilherme Gomes do Braz <ggbraz@gmail.com>
2021-11-01 23:25:30 -0300

    Add project files.

---
+++ ChangesInNetCore/MVCProject_6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCProject_6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

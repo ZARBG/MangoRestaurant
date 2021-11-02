8cd45911a90d44f90b27803f00d0c88f02d89d37
Guilherme Gomes do Braz <ggbraz@gmail.com>
2021-11-02 13:23:40 -0300

    Corrigindo o problema com o banco de dados.

--- MangoRestaurant/Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ MangoRestaurant/Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -33,6 +33,71 @@ namespace Mango.Services.ProductAPI.Controllers
             }
             return _response;
         }
+        
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<object> Get(int id)
+        {
+            try
+            {
+                ProductDto productDto = await _productRepository.GetProductById(id);
+                _response.Result = productDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost]
+        public async Task<object> POst([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                ProductDto model = await _productRepository.CreateUpdateProduct(productDto);
+                _response.Result = model;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+        
+        [HttpPut]
+        public async Task<object> Put([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                ProductDto model = await _productRepository.CreateUpdateProduct(productDto);
+                _response.Result = model;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+        
+        [HttpDelete]
+        public async Task<object> Delete(int productId)
+        {
+            try
+            {
+                bool isSuccess = await _productRepository.DeleteProduct(productId);
+                _response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
 
     }
 }

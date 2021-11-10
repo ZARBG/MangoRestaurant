0bf6e6d10331ccec8d8a8ff37bb6df57765fedb6
Guilherme Gomes do Braz <ggbraz@gmail.com>
2021-11-09 22:12:07 -0300

    Implementando as views de crud de produto.

--- MangoRestaurant/Mango.Services.ProductAPI/Models/Dto/ProductDto.cs
+++ MangoRestaurant/Mango.Services.ProductAPI/Models/Dto/ProductDto.cs
@@ -1,9 +1,12 @@
-﻿namespace Mango.Services.ProductAPI.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.ProductAPI.Models.Dto
 {
     public class ProductDto
     {
         public int ProductId { get; set; }
         public string Name { get; set; }
+        [Range(0.01, double.MaxValue)]
         public double Price { get; set; }
         public string Description { get; set; }
         public string CategoryName { get; set; }
--- MangoRestaurant/Mango.Web/Models/ProductDto.cs
+++ MangoRestaurant/Mango.Web/Models/ProductDto.cs
@@ -1,9 +1,12 @@
-﻿namespace Mango.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Web.Models
 {
     public class ProductDto
     {
         public int ProductId { get; set; }
         public string Name { get; set; }
+        [Range(0.01, double.MaxValue)]
         public double Price { get; set; }
         public string Description { get; set; }
         public string CategoryName { get; set; }
